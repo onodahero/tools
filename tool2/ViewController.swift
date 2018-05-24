accf4a86206c071fc4bbe5a691f1d99e00c144d2
hiroto onoda <onotakuhiroh0611@gmail.com>
2018-05-24 15:39:17 +0900

    Initial Commit

---
+++ tool2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tool2
+//
+//  Created by 斧田洋人 on 2018/05/24.
+//  Copyright © 2018年 斧田洋人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
